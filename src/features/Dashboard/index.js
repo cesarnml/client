1652dc33e24870360aae5a1cadff0b1b0c941fa5
Axolotl <samuelmachat@gmail.com>
2019-01-03 13:32:35 -0500

    introduce basic styling for dashboard

--- src/features/Dashboard/index.js
+++ src/features/Dashboard/index.js
@@ -6,6 +6,32 @@ import { fetchCollections } from './actions'
 // name, description, info
 // dashboard, new button up top (contingent on admin)
 
+const dashboardContainerStyles = {
+  border: '1px solid black'
+}
+
+const dashboardTitleStyles = {
+  position: 'relative'
+}
+
+const collectionsContainerStyles = {
+  display: 'flex',
+  flexDirection: 'column'
+}
+
+const collectionTileSyles = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  border: '1px solid black',
+  margin: '10px'
+}
+
+const infoTileStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start'
+}
+
 const mapStateToProps = ({ dashboard }) => ({
   dashboard
 })
@@ -19,19 +45,29 @@ class Dashboard extends Component {
     const { dashboard } = this.props
 
     return (
-      <div className="dashboard-container">
-        <h1>Dashboard</h1>
-        {dashboard
-          ? dashboard.map(collection => (
-              <div className="dashboard-collection-container">
-                <h2>{collection.collectionName}</h2>
-                <p>{collection.description}</p>
-                <p>Start: {collection.startDate}</p>
-                <p>End: {collection.endDate}</p>
-                <p>Max members per team: {collection.maxMembersPerTeam}</p>
-              </div>
-            ))
-          : null}
+      <div className="dashboard-container" style={dashboardContainerStyles}>
+        <div style={dashboardTitleStyles}>
+          <h1>Dashboard</h1>
+        </div>
+
+        <div style={collectionsContainerStyles}>
+          {dashboard
+            ? dashboard.map(collection => (
+                <div
+                  className="dashboard-collection-container"
+                  style={collectionTileSyles}
+                >
+                  <h2>{collection.collectionName}</h2>
+                  <div style={infoTileStyles}>
+                    <p>{collection.description}</p>
+                    <p>Start: {collection.startDate}</p>
+                    <p>End: {collection.endDate}</p>
+                    <p>Max members per team: {collection.maxMembersPerTeam}</p>
+                  </div>
+                </div>
+              ))
+            : null}
+        </div>
       </div>
     )
   }
