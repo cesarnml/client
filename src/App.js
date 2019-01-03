a76cfc7954a66319d1f1e556c964ffde832d6bb2
Axolotl <samuelmachat@gmail.com>
2019-01-03 18:01:09 -0500

    reverse app.js to original

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { fetchUser } from './features/Authentication/authActions'
 import NavBar from './features/NavBar'
 import Collection from './features/Collection'
 import Login from './features/Login'
-import Dashboard from './features/Dashboard'
 import './styles/App.css'
 
 const AuthLandingPage = requireAuth(Login)
@@ -24,7 +23,7 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar />
-        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/" component={AuthLandingPage} />
 
         {/* collection compoment will take an id. it renders all the projects for a collection */}
         <Route
