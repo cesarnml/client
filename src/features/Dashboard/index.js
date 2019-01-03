f5915cb08e401ca4377aecc616da63e2f072a519
Axolotl <samuelmachat@gmail.com>
2019-01-03 11:54:56 -0500

    dashboard loading with mock data

--- src/features/Dashboard/index.js
+++ src/features/Dashboard/index.js
@@ -1,5 +1,44 @@
 import React from 'react'
 
-const Dashboard = () => <p>Dashboard goes here</p>
+// name, description, info
+// dashboard, new button up top (contingent on admin)
+
+const mockData = [
+  {
+    collectionName: 'collection 1',
+    description: ';lkafleakh;kajge',
+    startDate: 'now',
+    endDate: 'then',
+    maxMembersPerTeam: 5
+  },
+  {
+    collectionName: 'collection 2',
+    description: ';lkafleakh;kajge',
+    startDate: 'now',
+    endDate: 'then',
+    maxMembersPerTeam: 5
+  },
+  {
+    collectionName: 'collection 3',
+    description: ';lkafleakh;kajge',
+    startDate: 'now',
+    endDate: 'then',
+    maxMembersPerTeam: 5
+  }
+]
+
+const Dashboard = () => (
+  <div className="dashboard-container">
+    {mockData.map(collection => (
+      <div className="dashboard-collection-container">
+        <h2>{collection.collectionName}</h2>
+        <p>{collection.description}</p>
+        <p>Start: {collection.startDate}</p>
+        <p>End: {collection.endDate}</p>
+        <p>Max members per team: {collection.maxMembersPerTeam}</p>
+      </div>
+    ))}
+  </div>
+)
 
 export default Dashboard
