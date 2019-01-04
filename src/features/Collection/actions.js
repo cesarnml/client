d3c7978e1d328ff280cb98b27bf6627691d8bad4
Axolotl <samuelmachat@gmail.com>
2019-01-04 12:48:00 -0500

    getting member data from server and displaying

--- src/features/Collection/actions.js
+++ src/features/Collection/actions.js
@@ -19,5 +19,17 @@ export const fetchCollection = id => async dispatch => {
   const res = await axios.get(
     `${process.env.REACT_APP_SERVER_URL}/api/collections/${id}/projects`
   )
-  dispatch({ type: FETCH_COLLECTION, id, payload: res.data })
+
+  const members = await axios.get(
+    `${process.env.REACT_APP_SERVER_URL}/api/collections/${id}/projects/members`
+  )
+
+  const joinedRes = res.data.map(project => ({
+    ...project,
+    members: members.data
+      .filter(member => project.id === member.projectId)
+      .map(({ userDisplayName }) => userDisplayName)
+  }))
+
+  dispatch({ type: FETCH_COLLECTION, id, payload: joinedRes })
 }
